31a248a7e5a3b3656221fe0706fb3884316edec5
hirose0320 <momotamomota0118@gmail.com>
2019-02-14 17:44:12 +0900

    java

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("Hello World");
+
+
+		int i=0;
+
+		do{
+			System.out.println(i);
+			i++;
+		}while(i<0);
+
+	}
+
+}
