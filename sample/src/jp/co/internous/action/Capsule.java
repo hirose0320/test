66f04360df3b9d40b1ec538d5562ba63aae060f5
hirose0320 <momotamomota0118@gmail.com>
2019-02-15 15:12:18 +0900

    215

---
+++ sample/src/jp/co/internous/action/Capsule.java
@@ -0,0 +1,19 @@
+package jp.co.internous.action;
+
+public class Capsule {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Person3 taro = new Person3("山田太郎",20);
+		System.out.println(taro.getName());
+		taro.setName("花子");
+		System.out.println(taro.getName());
+
+		System.out.println(taro.getAge());
+		taro.setAge(30);
+		System.out.println(taro.getAge());
+
+	}
+
+}
